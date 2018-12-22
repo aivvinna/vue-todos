4798a8bf211e2401a742fe26015935876aec3b83
Hiroki Furuichi <aivvinna@gmail.com>
2018-12-22 19:41:39 +0900

    added simple registration page. no functionality yet

---
+++ src/views/Register.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input
+      type="email"
+      name="email"
+      placeholder="email">
+    <br>
+    <input
+      type="password"
+      name="password"
+      placeholder="password">
+    <br>
+    <button>Register</button>
+  </div>
+</template>
